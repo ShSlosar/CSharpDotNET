3a74600f972eebee62c7714be3b6933ef4802361
Shelby Slosar <shelby.slosar@protonmail.com>
2023-02-02 15:29:33 -0800

    Var_types

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -2,8 +2,11 @@
 {
     private static void Main(string[] args)
     {
-        string place = "dealer";
-        string message = $"hello {place}";
+        string greeting = "how are you doing today?";
+        string place = "Sarah";
+        int age = 27;
+        bool blueEyed = true;
+        string message = $"hello {place}, {greeting}, you are {age}, and it is {blueEyed} that you have blue eyes.";
         Console.WriteLine(message);
     }
 }
