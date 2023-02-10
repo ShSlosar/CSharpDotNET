59e3c0bf5c20f395beec31a8095633d55bf03dcf
Shelby Slosar <shelby.slosar@protonmail.com>
2023-02-10 10:39:35 -0800

    conditionals

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -5,8 +5,15 @@
         string greeting = "how are you doing today?";
         string place = "Sarah";
         int age = 27;
+        int numRings = 5;
         bool blueEyed = true;
         string message = $"hello {place}, {greeting}, you are {age}, and it is {blueEyed} that you have blue eyes.";
         Console.WriteLine(message);
+        if (numRings >= 5){
+            Console.WriteLine($"Welcome to the party {place}!");
+        }
+        else{
+            Console.WriteLine($"Sorry, {place}, you need more rings to get in!");
+        }
     }
 }
