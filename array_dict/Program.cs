42d51fdd24c27142e4deaa63f9362bd19116226a
Shelby Slosar <shelby.slosar@protonmail.com>
2023-02-11 09:37:05 -0800

    arrays&loops

---
+++ array_dict/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] numArray = new int[5];
+        int[] arrayOfInts = {1,2,3,4,5};
+        int[] arr = {1,2,3,4};
+        Console.WriteLine($"index 1 of array is {arr[0]}.");
+        arr[0] = 8;
+        Console.WriteLine($"index 1 of array is {arr[0]}.");
+
+        //foreach loop, string array
+        string[] myCars = new string[] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"}; 
+        foreach (string car in myCars){
+            Console.WriteLine($"I own a {car}");
+            }
+
+
+    }
+}
